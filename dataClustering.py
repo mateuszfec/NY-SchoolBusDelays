658d08fab5edc1b4637f2daa3772b7b01de838d6
Mateusz Fec <mateuszfec@gmail.com>
2019-05-13 21:33:16 +0200

    Most of variables are changed into dummy type

--- dataClustering.py
+++ dataClustering.py
@@ -60,21 +60,35 @@ dataset["Reaction_Time"] = dataset["Created_On_Time"]-dataset["Occurred_On_Time"
 # Remove unused data
 dataset.drop(["Incident_Number",
               "Last_Updated_On",
+              "Busbreakdown_ID",
+              "Schools_Serviced",
               "Occurred_On", "Occurred_On_Time",
               "Created_On", "Created_On_Time", "Created_On_Date",
               "Informed_On", "Informed_On_Date"], axis=1, inplace=True)
 
 # Convert categorical into dummy variables
 dataset = variableToDummies(dataset, "Reason", "Delay_Reason", True)
+dataset = variableToDummies(dataset, "School_Age_or_PreK", "Education_level", True)
+dataset = variableToDummies(dataset, "Breakdown_or_Running_Late", "Delay_cause", True)
+dataset = variableToDummies(dataset, "Bus_Company_Name", "Bus_Company", False) # TODO: Check bus company names (duplicated)
+dataset = variableToDummies(dataset, "Boro", "NY_Boro", False) # TODO: Check exception on print data desc
+dataset = variableToDummies(dataset, "Route_Number", "NY_Route", False)
+dataset = variableToDummies(dataset, "Bus_No", "Bus_Number", False)
+dataset = variableToDummies(dataset, "Run_Type", "Bus_Run_Type", True)
+dataset = variableToDummies(dataset, "School_Year", "NY_School_Year", False)
 # TODO: convert rest of categorical variables into dummy ;)
 
 dataset[["Schools_NOT_Notified", "Schools_Notified"]] = pd.get_dummies(dataset['Has_Contractor_Notified_Schools'])
 dataset[["Parents_NOT_Notified", "Parents_Notified"]] = pd.get_dummies(dataset['Has_Contractor_Notified_Parents'])
+dataset[["OPT_NOT_Alerted", "OPT_Alerted"]] = pd.get_dummies(dataset['Have_You_Alerted_OPT'])
 
 # Remove categorical variables
 dataset.drop(["Has_Contractor_Notified_Schools", "Schools_NOT_Notified",
-              "Has_Contractor_Notified_Parents", "Parents_NOT_Notified"], axis=1, inplace=True)
+              "Has_Contractor_Notified_Parents", "Parents_NOT_Notified",
+              "Have_You_Alerted_OPT", "OPT_NOT_Alerted"], axis=1, inplace=True)
 
+# Delays time preprocessing
+# TODO: Preprocessing column with time of delays
 # ---------------------------------------------------- K-Means ---------------------------------------------------------
 # TODO: k-means clustering
 
