77a9705f658dde6b965ccff934bbe2774cde2b8f
Mateusz Fec <mateuszfec@gmail.com>
2019-05-16 21:25:56 +0200

    Bus_Delay times are calculated

--- dataClustering.py
+++ dataClustering.py
@@ -1,5 +1,7 @@
 # Libraries
 import pandas as pd
+import numpy as np
+import re
 
 # -------------------------------------------------- Functions ---------------------------------------------------------
 # Convert categorical variable into dummies
@@ -27,8 +29,9 @@ def variableToDummies(dataset, colname, finalName, legend):
     tempdummies.fillna(0, inplace=True)
     dataset.drop([colname], axis=1, inplace=True)
     dataset[finalName] = tempdummies["temp_summary_272727"]
-
+    # TODO: Check is possible to return column with datatype (int32)
     return dataset
+
 # String multi-replacement
 def multipleReplace(dataset, colname, phraseList, replace):
     for key in phraseList:
@@ -117,14 +120,103 @@ dataset[["School_Year", "School_Level", "Delay_Reason", "Delay_Result", "Boro", 
 dataset["Occurred_On"] = pd.to_datetime(dataset["Occurred_On"], format="%H:%M:%S").dt.time
 dataset["Informed_On"] = pd.to_datetime(dataset["Informed_On"], format="%H:%M:%S").dt.time
 
-# Delays data preprocessing
-# TODO: Set-up the final data of the bus delays
+# Delays data preprocessing (hours)
+hours = dataset['How_Long_Delayed'].str.split('(h|H)', expand=True)
+hours.fillna(0, inplace=True)
+hours['hours_summary'] = hours[0].where(hours[1] != 0, 0).str.strip()
+dataset['Delay_From_Hours'] = hours['hours_summary']
+del hours
+
+multipleReplace(dataset, "Delay_From_Hours", {'0', '45 -1', "45-1", ''}, "0")
+multipleReplace(dataset, "Delay_From_Hours", {'1/2', '30MINS-1'}, "30")
+multipleReplace(dataset, "Delay_From_Hours", {'1', '1-', "1:00", "1:", "one", "ONE", 'I', "\\1", "MORE T", '45minTO 1'}, "60")
+multipleReplace(dataset, "Delay_From_Hours", {'1 1/2', '1.5', "1:30", "30MINS-1"}, "90")
+multipleReplace(dataset, "Delay_From_Hours", {'40-1', "40/1"}, "100")
+multipleReplace(dataset, "Delay_From_Hours", {'1:45', "45 MIN/1", "45 min-1", "45/1", "451", "45MIN-1",
+                                        "45min -1", '45min - 1', "45min-1", "45min/1", "45mini/1", "45mins 1"}, "105")
+multipleReplace(dataset, "Delay_From_Hours", {'50-1', "50min 1"}, "110")
+multipleReplace(dataset, "Delay_From_Hours", {'2', "2.0", "2:"}, "120")
+multipleReplace(dataset, "Delay_From_Hours", {'3'}, "180")
+multipleReplace(dataset, "Delay_From_Hours", {'4'}, "240")
+dataset["Delay_From_Hours"].fillna(0, inplace=True)
+dataset["Delay_From_Hours"] = dataset["Delay_From_Hours"].astype(int)
+# test = variableToDummies(dataset, "full_hours", "test_sample", True)
+
+# Delays data preprocessing (minutes)
+minutes = dataset['How_Long_Delayed'].str.split('(m|M)', expand=True)
+minutes.fillna(0, inplace=True)
+minutes['minutes_stripped'] = minutes[0].where(minutes[1] != 0, 0).str.strip(' AaPp:+-_,n!?*&?SONiol.')
+minutes.fillna(0, inplace=True)
+
+r = re.compile(r'[0-9].*(-|:).*[0-9]')
+regmatch = np.vectorize(lambda x: bool(r.match(x)))
+minutes['minutes_condition'] = minutes[0].astype(str, copy=True, errors='raise')
+minutes['minutes_condition_bool'] = regmatch(minutes['minutes_condition'].values)
+
+minutes['minutes_summary'] = minutes["minutes_condition"].where(
+    (minutes['minutes_stripped'] == 0) & minutes['minutes_condition_bool'] == True, minutes['minutes_stripped'])
+# test = variableToDummies(minutes, 'minutes_condition', "test_sample", True)
+
+multipleReplace(minutes, "minutes_summary", {'1:40', '1:20', '1:40', '1:30'}, "2")
+multipleReplace(minutes, "minutes_summary", {'02:19'}, "3")
+multipleReplace(minutes, "minutes_summary", {'3.5'}, "4")
+multipleReplace(minutes, "minutes_summary", {'07:41', '8:07', '7:29'}, "8")
+multipleReplace(minutes, "minutes_summary", {'25:30'}, "26")
+multipleReplace(minutes, "minutes_summary", {'10 15', '10*-15'}, "10-15")
+multipleReplace(minutes, "minutes_summary", {'25  30'}, "20-30")
+multipleReplace(minutes, "minutes_summary", {'15-*20', '15*-20', 'est.15-20'}, "15-20")
+multipleReplace(minutes, "minutes_summary", {'20 dart'}, "20")
+multipleReplace(minutes, "minutes_summary", {'20in/30', 'Est.20-30'}, "20-30")
+multipleReplace(minutes, "minutes_summary", {'IHR40'}, "40")
+multipleReplace(minutes, "minutes_summary", {'40-1 hour', '40-1HR', '40-1hr'}, "40-60")
+multipleReplace(minutes, "minutes_summary", {'45 -1 hour', '45 -1hour', '45-1 HR', '45-1HR', '45-1hour', '45-1hr'}, "45-60")
+multipleReplace(minutes, "minutes_summary", {'50-1 hour', '50-1HR', '50-1hr'}, "50-60")
+multipleReplace(minutes, "minutes_summary", {'1hr-1 1/2'}, "60-90")
+multipleReplace(minutes, "minutes_summary", {'1hr - 1:45'}, "60-105")
+multipleReplace(minutes, "minutes_summary", {'est. 12'}, "12")
+multipleReplace(minutes, "minutes_summary", {'est. 20'}, "20")
+multipleReplace(minutes, "minutes_summary", {'est. 25'}, "25")
+multipleReplace(minutes, "minutes_summary", {'est. 30'}, "30")
+multipleReplace(minutes, "minutes_summary", {'est. 35'}, "35")
+multipleReplace(minutes, "minutes_summary", {'est.20'}, "20")
+multipleReplace(minutes, "minutes_summary", {'est.35'}, "35")
+multipleReplace(minutes, "minutes_summary", {'est.9:15'}, "10")
+multipleReplace(minutes, "minutes_summary", {'0', '', '1 20', '1 30', '1 HOUR 15', '1 HR 30', '1hr 15', '1hr 30',
+                                             '1H15', '1HOUR15', '1HR 30', '1HR 45', '1HR/20', '1HR20', '1hour 20',
+                                             '1hour 30', '1hour45', '1hr /30', '1hr 10', '1hr 15', '1hr 20', '1hr 30',
+                                             '1hr 40', '1hr 45', '1hr&30', '1hr. 20', '1hr/', '1hr/15', '1hr/20',
+                                             '1hr/30', '1hr20', '1hr45', '1hrs 20', '1 hr 15', '1 hr 30', '1:00',
+                                             '1:00 h', '1:00 hour', '1:30 H/', '1:30 HR', '1:30 h', '1:45hr',
+                                             '1hr - 11/2'}, "0")
+# test = variableToDummies(minutes, "minutes_summary", "test_sample", True)
+
+minutes["minutesBasic"] = pd.to_numeric(minutes[0], errors='coerce')
+minutes["minutesBasic"].fillna(0, inplace=True)
+
+minutesAverage = minutes['minutes_summary'].str.split('(-|/|to)', expand=True)
+minutesAverage[0] = minutesAverage[0].str.strip()
+minutesAverage[2] = minutesAverage[2].str.strip()
+minutesAverage.fillna(0, inplace=True)
+minutesAverage[0] = minutesAverage[0].astype(int)
+multipleReplace(minutesAverage, 2, {''}, "0")
+minutesAverage[2] = minutesAverage[2].astype(int)
+# test = variableToDummies(test, 2, "test_sample", True)
+
+dataset["Delay_From_Minutes"] = minutes["minutesBasic"].where(minutesAverage[2] == 0)
+dataset["Avg_From_Minutes"] = ((minutesAverage[0] + minutesAverage[2])/2).where(minutesAverage[2] != 0)
+dataset["Delay_From_Minutes"].fillna(0, inplace=True)
+dataset["Avg_From_Minutes"].fillna(0, inplace=True)
+del minutes, minutesAverage, r, regmatch
+
+dataset["Bus_Delay"] = dataset["Delay_From_Hours"] + dataset["Delay_From_Minutes"] + dataset["Avg_From_Minutes"]
+dataset["Bus_Delay"] = dataset["Bus_Delay"].astype(int)
+dataset.drop(["How_Long_Delayed", "Delay_From_Hours", "Delay_From_Minutes", "Avg_From_Minutes"], axis=1, inplace=True)
 
 # Final dataset structure
 dataset = dataset.rename(columns={'Number_Of_Students_On_The_Bus': 'Students_Number'})
 dataset["Students_Number"] = dataset["Students_Number"].astype(int)
 
-dataset = dataset[['How_Long_Delayed',
+dataset = dataset[['Bus_Delay',
                    'Event_Date',
                    'Occurred_On',
                    'Informed_On',
@@ -144,6 +236,9 @@ dataset = dataset[['How_Long_Delayed',
                    'Parents_Notified']]
 
 # print(dataset.dtypes)
+
+# ------------------------------------------------- Data analysis ------------------------------------------------------
+# TODO: Basic statistics, remove outliers
 
 # ---------------------------------------------------- K-Means ---------------------------------------------------------
 # TODO: k-means clustering
